bfc8aae0259e5e883e8a46b594791142190d3249
Kuldeep <kunalrajput67718@gmail.com>
2025-11-28 16:43:26 +0530

    added welcome section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "#components/Navbar"
+import Welcome from "#components/Welcome"
 
 
 const App = () => {
   return (
     <main>
       <Navbar/>
+      <Welcome/>
     </main>
 )
 }

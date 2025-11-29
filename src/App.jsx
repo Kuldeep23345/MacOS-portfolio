951bd4f9ac9360bffcb74d4e4a6424d877a924f1
Kuldeep <kunalrajput67718@gmail.com>
2025-11-29 21:47:56 +0530

    gallery implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,37 @@
-import Dock from "#components/Dock"
-import Navbar from "#components/Navbar"
-import Welcome from "#components/Welcome"
-import Finder from "#windows/Finder"
-import Resume from "#windows/Resume"
-import Safari from "#windows/Safari"
-import Terminal from "#windows/Terminal"
-import Text from "#windows/Text"
-import Image from "#windows/Image"
-import Contact from "#windows/Contact"
-import gsap from "gsap"
+import Dock from "#components/Dock";
+import Navbar from "#components/Navbar";
+import Welcome from "#components/Welcome";
+import Finder from "#windows/Finder";
+import Resume from "#windows/Resume";
+import Safari from "#windows/Safari";
+import Terminal from "#windows/Terminal";
+import Text from "#windows/Text";
+import Image from "#windows/Image";
+import Contact from "#windows/Contact";
+import Home from "#components/Home";
+import Photos from "#windows/Photos";
+import gsap from "gsap";
 
-import { Draggable } from "gsap/Draggable"
-import Home from "#components/Home"
-gsap.registerPlugin(Draggable)
-
+import { Draggable } from "gsap/Draggable";
+gsap.registerPlugin(Draggable);
 
 const App = () => {
   return (
     <main>
-      <Navbar/>
-      <Welcome/>
-      <Dock/>
-      <Terminal/>
-      <Safari/>
-      <Resume/>
-      <Finder/>
-      <Text/>
-      <Image/>
-      <Contact/>
-      <Home/>
+      <Navbar />
+      <Welcome />
+      <Dock />
+      <Terminal />
+      <Safari />
+      <Resume />
+      <Finder />
+      <Text />
+      <Image />
+      <Contact />
+      <Photos />
+      <Home />
     </main>
-)
-}
+  );
+};
 
-export default App
+export default App;
